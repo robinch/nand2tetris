5b9958795dcb591d6f0ec007ddefd9b1508dc5bd
Robin Chowdhury <robinch88@gmail.com>
2023-03-25 19:40:59 +0100

    add integration test for Add

--- projects/06/hack_assembler/test/hack_assembler_test.exs
+++ projects/06/hack_assembler/test/hack_assembler_test.exs
@@ -4,5 +4,11 @@ defmodule HackAssemblerTest do
 
   test "Add" do
     assert :ok = HackAssembler.assemble("test/support/Add.asm")
+    expected = File.stream!("test/support/Add_expected.hack")
+    result = File.stream!("test/support/Add.hack")
+
+    for {exp, res} <- Enum.zip(expected, result) do
+      assert exp == res
+    end
   end
 end
