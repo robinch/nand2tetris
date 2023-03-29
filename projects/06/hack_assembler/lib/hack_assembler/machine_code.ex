60cc902a2c8da73c6fd174c0cb45ed00b424b6e3
Robin Chowdhury <robinch88@gmail.com>
2023-03-29 23:16:06 +0200

    c instruction to machine code

--- projects/06/hack_assembler/lib/hack_assembler/machine_code.ex
+++ projects/06/hack_assembler/lib/hack_assembler/machine_code.ex
@@ -8,6 +8,78 @@ defmodule HackAssembler.MachineCode do
     "000" <> bin_address
   end
 
+  def c_instruction(dest, comp, jump) do
+    prefix = "111"
+    a = String.contains?(comp, "M") |> bool_to_bit()
+    dest = dest(dest)
+    comp = comp(comp)
+    jump = jump(jump)
+
+    prefix <> a <> comp <> dest <> jump
+  end
+
+  defp dest(dest) do
+    case dest do
+      nil -> "000"
+      "M" -> "001"
+      "D" -> "010"
+      "DM" -> "011"
+      "A" -> "100"
+      "AM" -> "101"
+      "AD" -> "110"
+      "ADM" -> "000"
+    end
+  end
+
+  def jump(jump) do
+    case jump do
+      nil -> "000"
+      "JGT" -> "001"
+      "JEQ" -> "010"
+      "JGE" -> "011"
+      "JLT" -> "100"
+      "JNE" -> "101"
+      "JLE" -> "110"
+      "JMP" -> "000"
+    end
+  end
+
+  def comp(comp) do
+    case comp do
+      "0" -> "101010"
+      "1" -> "111111"
+      "-1" -> "111010"
+      "D" -> "001100"
+      "A" -> "110000"
+      "M" -> "110000"
+      "!D" -> "001101"
+      "!A" -> "110001"
+      "!M" -> "110001"
+      "-D" -> "001111"
+      "-A" -> "110011"
+      "-M" -> "110011"
+      "D+1" -> "011111"
+      "A+1" -> "110111"
+      "M+1" -> "110111"
+      "D-1" -> "001110"
+      "A-1" -> "110010"
+      "M-1" -> "110010"
+      "D+A" -> "000010"
+      "D+M" -> "000010"
+      "D-A" -> "010011"
+      "D-M" -> "010011"
+      "A-D" -> "000111"
+      "M-D" -> "000111"
+      "D&A" -> "000000"
+      "D&M" -> "000000"
+      "D|A" -> "010101"
+      "D|M" -> "010101"
+    end
+  end
+
+  defp bool_to_bit(true), do: "1"
+  defp bool_to_bit(false), do: "0"
+
   defp to_binary(number) when is_binary(number) do
     val = String.to_integer(number)
 
--- projects/06/hack_assembler/test/hack_assembler/machine_code_test.exs
+++ projects/06/hack_assembler/test/hack_assembler/machine_code_test.exs
@@ -6,4 +6,8 @@ defmodule HackAssembler.MachineCodeTest do
   test "A instruction" do
     assert MachineCode.a_instruction("123") == "0000000001111011"
   end
+
+  test "C instruction" do
+    assert MachineCode.c_instruction("AD", "D+1", "JEQ") == "1110011111110010"
+  end
 end
